57065408765fcb20ddda0c30d1b3c5701b67df6e
jeanette Wilson <jayem.wilson@gmail.com>
2017-06-28 19:49:25 -0400

    pets

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
-  get 'welcome/pets'
+  
+  get 'welcome/home'
 
   get 'welcome/supplies'
+
+  resources :pets
+  root 'welcome#home'  
+    
+  get 'home' => 'welcome#home'
+
+  get 'home' => 'welcome#home'
+
+  get  'supplies' => 'welcome#supplies'
+
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get pets" do
-    get welcome_pets_url
+  test "should get home" do
+    get welcome_home_url
     assert_response :success
   end
 
